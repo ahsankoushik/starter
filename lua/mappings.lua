91b7e0ba692f89922628a993b7d15d7364de590a
ahsankoushik <md.ahsan.hasib@g.bracu.ac.bd>
2025-05-28 08:10:07 +0600

    git files added to cp

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,6 @@
 require "nvchad.mappings"
 
+local builtin = require('telescope.builtin')
 -- add yours here
 
 local map = vim.keymap.set
@@ -8,3 +9,5 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
